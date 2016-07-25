d14f28793c101ef862998d13f0cb73fa125c17b6
sriramvenkatasaibharath <bharathsaisriram@gmail.com>
2016-07-25 17:30:59 +0530

    Update Neg_Or_Pos.c

--- Neg_Or_Pos.c
+++ Neg_Or_Pos.c
@@ -5,11 +5,17 @@ void main()
  int num;
   scanf("%d",&num)
  if(num>0)
+ {
+  printf("%d is Positive\n",num);
+ printf("%d",num%100);
+ }
+ else if(num<0)
 {
-printf("%d",num%100);
+ printf("%d is Negative\n",num);
+ printf("%d",0);
 }
-else if(num<0)
-{
-printf("%d",0);
-}
+else
+printf("Zero")
+
+
 }
