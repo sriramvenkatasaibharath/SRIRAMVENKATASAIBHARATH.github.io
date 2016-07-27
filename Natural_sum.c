582e421255b3302d24291677de3ad1d2bbb1fb76
sriramvenkatasaibharath <bharathsaisriram@gmail.com>
2016-07-27 21:53:58 +0530

    Add files via upload

---
+++ Natural_sum.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int no,i,sum=0;
+	printf("\nEnter a number");
+	scanf("%d",&no);
+	for(i=0;i<n;i++)
+	sum=sum+1;
+	printf("\n%d",no);
+	return 0;
+}
