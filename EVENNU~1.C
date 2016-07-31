f7ac7e2773d827c8c43f5c4c6c7f2f548d7d8947
sriramvenkatasaibharath <bharathsaisriram@gmail.com>
2016-07-31 23:55:30 +0530

    Add files via upload

---
+++ EVENNU~1.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int upper,lower,i;
+printf("enter the value upperand lower");
+scanf("%d%d",&lower,&upper);
+for(i=lower;i<upper;i++)
+{
+if(i%2==0)
+{
+printf("%d ",i);
+}
+}
+getch();
+}
