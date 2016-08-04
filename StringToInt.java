5a7910f05063e780f412968425e12d1c094398f6
sriramvenkatasaibharath <bharathsaisriram@gmail.com>
2016-08-04 14:51:46 +0530

    Add files via upload

---
+++ StringToInt.java
@@ -0,0 +1,17 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner scan = new Scanner(System.in);
+		String str = scan.nextLine();
+		int i = Integer.parseInt(str);
+		System.out.println(i);
+	}
+}
