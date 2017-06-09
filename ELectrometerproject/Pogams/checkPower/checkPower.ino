bd266f3ff39381a5263ca49980a656c9bd20f234
sriramvenkatasaibharath <bharathsaisriram@gmail.com>
2017-06-09 20:57:58 +0530

    Add files via upload

---
+++ ELectrometerproject/Pogams/checkPower/checkPower.ino
@@ -0,0 +1,248 @@
+#include <WiFi.h>
+#include <SPI.h> 
+ // ThingSpeak Settings
+char thingSpeakAddress[] = "api.thingspeak.com";  //TS
+String writeAPIKey = "UPLGHMMWYRKDR20D";//TS
+int updateThingSpeakInterval = 1 * 1000;      // Time interval in milliseconds to update ThingSpeak (number of seconds * 1000 = interval)
+int status = WL_IDLE_STATUS;     // the Wifi radio's status
+double Voltage = 0;
+double Voltage_1 = 0;
+double VRMS = 0;
+double AmpsRMS = 0;
+double VRMS_1 = 0;
+double AmpsRMS_1 = 0;
+int power =0;
+int power_1 =0;
+const int sensorIn = A0;
+const int sensorIn_1 = A1;
+int mVperAmp = 100; // use 100 for 20A Module and 66 for 30A Module
+
+// initialize the library instance:
+WiFiClient client;
+// Variable Setup
+long lastConnectionTime = 0; 
+boolean lastConnected = false;
+int failedCounter = 0;
+int a;
+int B=3975; 
+float ctemp;
+float res;
+
+void setup() {
+  //Initialize serial and wait for port to open:
+  Serial.begin(9600); 
+  while (!Serial)
+{
+    ; // wait for serial port to connect. Needed for Leonardo only
+  }
+  
+  // you're connected now, so print out the data:
+  Serial.print("You're connected to the network");
+  printCurrentNet();
+  printWifiData();
+}
+void loop() {
+  // check the network connection once every 10 seconds:
+  lastConnectionTime = 0;
+  updateThingSpeakInterval = 1000;  
+ //calculation of power for two different locations  
+ Voltage   = getVPP();
+ VRMS = (Voltage/2.0) *0.707; 
+ AmpsRMS = (VRMS * 1000)/mVperAmp;
+ power=(230*AmpsRMS);
+
+Voltage_1   = getVPP_1();
+ VRMS_1 = (Voltage_1/2.0) *0.707; 
+ AmpsRMS_1 = (VRMS_1 * 1000)/mVperAmp;
+ power_1=(230*AmpsRMS_1);
+    
+ String analogPin0 = String(power);
+ String analogPin_1 = String(power_1);
+
+ // Print Update Response to Serial Monitor
+  if (client.available())
+  {
+    char c = client.read();
+    Serial.print(c);
+  }
+
+  // Disconnect from ThingSpeak
+  if (!client.connected() && lastConnected)
+  {
+    Serial.println("...disconnected");
+    Serial.println();
+    client.stop();
+  }
+
+  // Update ThingSpeak
+  if(!client.connected() && (lastConnectionTime < updateThingSpeakInterval))
+  {
+      Serial.print("\t Amps = ");
+      updateThingSpeak("field2="+analogPin0+"&field1="+analogPin_1);
+      delay(20000);  
+      client.stop();
+  }
+  
+  lastConnected = client.connected();
+ 
+}
+
+void updateThingSpeak(String tsData)
+{
+  if (client.connect(thingSpeakAddress, 80))
+  {         
+    client.print("POST /update HTTP/1.1\n");
+    client.print("Host: api.thingspeak.com\n");
+    client.print("Connection: close\n");
+    client.print("X-THINGSPEAKAPIKEY: "+writeAPIKey+"\n");
+    client.print("Content-Type: application/x-www-form-urlencoded\n");
+    client.print("Content-Length: ");
+    client.print(tsData.length());
+    client.print("\n\n");
+    client.print(tsData);
+  //  client.print(tsData_1);
+    
+    lastConnectionTime = millis();
+          Serial.println(lastConnectionTime);
+
+    if (client.connected())
+    {
+      Serial.println("Connecting to ThingSpeak...");
+      Serial.println();
+      failedCounter = 0;
+    }
+    else
+    {
+      failedCounter++;
+      Serial.println("Connection to ThingSpeak failed ("+String(failedCounter, DEC)+")");   
+      Serial.println();
+    }
+    
+  }
+  else
+  {
+    failedCounter++;
+    Serial.println("Connection to ThingSpeak Failed ("+String(failedCounter, DEC)+")");   
+    Serial.println();
+    lastConnectionTime = millis(); 
+  }
+  //lastConnectionTime = 0;
+}
+
+void printWifiData() {
+  // print your WiFi shield's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+  Serial.println(ip);
+  
+  // print your MAC address:
+  byte mac[6];  
+  WiFi.macAddress(mac);
+  Serial.print("MAC address: ");
+  Serial.print(mac[5],HEX);
+  Serial.print(":");
+  Serial.print(mac[4],HEX);
+  Serial.print(":");
+  Serial.print(mac[3],HEX);
+  Serial.print(":");
+  Serial.print(mac[2],HEX);
+  Serial.print(":");
+  Serial.print(mac[1],HEX);
+  Serial.print(":");
+  Serial.println(mac[0],HEX);
+ 
+}
+
+void printCurrentNet() {
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print the MAC address of the router you're attached to:
+  byte bssid[6];
+  WiFi.BSSID(bssid);    
+  Serial.print("BSSID: ");
+  Serial.print(bssid[5],HEX);
+  Serial.print(":");
+  Serial.print(bssid[4],HEX);
+  Serial.print(":");
+  Serial.print(bssid[3],HEX);
+  Serial.print(":");
+  Serial.print(bssid[2],HEX);
+  Serial.print(":");
+  Serial.print(bssid[1],HEX);
+  Serial.print(":");
+  Serial.println(bssid[0],HEX);
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.println(rssi);
+
+  // print the encryption type:
+  byte encryption = WiFi.encryptionType();
+  Serial.print("Encryption Type:");
+  Serial.println(encryption,HEX);
+  Serial.println();
+}
+float getVPP()
+{
+  float result;
+  
+  int readValue;             //value read from the sensor
+  int maxValue = 0;          // store max value here
+  int minValue = 1024;          // store min value here
+  
+   uint32_t start_time = millis();
+   while((millis()-start_time) < 1000) //sample for 1 Sec
+   {
+       readValue = analogRead(sensorIn);
+       // see if you have a new maxValue
+       if (readValue > maxValue) 
+       {
+           /*record the maximum sensor value*/
+           maxValue = readValue;
+       }
+       if (readValue < minValue) 
+       {
+           /*record the maximum sensor value*/
+           minValue = readValue;
+       }
+   }
+   
+   // Subtract min from max
+   result = ((maxValue - minValue) * 5.0)/1024.0;
+      
+   return result;
+ }
+ float getVPP_1()
+{
+  float result;
+  
+  int readValue;             //value read from the sensor
+  int maxValue = 0;          // store max value here
+  int minValue = 1024;          // store min value here
+  
+   uint32_t start_time = millis();
+   while((millis()-start_time) < 1000) //sample for 1 Sec
+   {
+       readValue = analogRead(sensorIn_1);
+       // see if you have a new maxValue
+       if (readValue > maxValue) 
+       {
+           /*record the maximum sensor value*/
+           maxValue = readValue;
+       }
+       if (readValue < minValue) 
+       {
+           /*record the maximum sensor value*/
+           minValue = readValue;
+       }
+   }
+   
+   // Subtract min from max
+   result = ((maxValue - minValue) * 5.0)/1024.0;
+      
+   return result;
+}
